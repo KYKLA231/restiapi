9dac265632d3b1a3ac1f5d4097dbf5465eb50a89
.alisoon <152703768+alisoon14@users.noreply.github.com>
2025-11-28 14:51:42 +0300

    Delete app directory

--- app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
-SECRET_KEY = os.getenv("SECRET_KEY", "change-me-please-use-env")
-ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "60"))
